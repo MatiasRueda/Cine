100509be170ac005d9e832f3bbddb66ed35c4377
MatiasRueda <matiasrued@gmail.com>
2023-07-03 02:58:24 -0300

    GitIgnore agregado

--- src/main/java/cine/App.java
+++ src/main/java/cine/App.java
@@ -2,7 +2,6 @@ package cine;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -17,14 +16,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 640, 480);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login" + ".fxml"));
+        scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setScene(scene);
         stage.show();
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
