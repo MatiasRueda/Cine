72e60b6fbac6b81cd43b65cbb65da1cfc0edddd4
MatiasRueda <matiasrued@gmail.com>
2023-07-20 13:55:05 -0300

    Pantalla de carga corregida

--- src/main/java/cine/controller/ComprarController.java
+++ src/main/java/cine/controller/ComprarController.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -25,8 +27,16 @@ public class ComprarController {
     @FXML
     void pelicula(ActionEvent event) throws IOException, InterruptedException {
         Parent pantallaCarga = Escenas.loadFXML("carga");
+        Stage primaryStage = (Stage)comprar.getScene().getWindow();
         Scene sceneCarga = new Scene(pantallaCarga);
         Stage secundaryStage = new Stage();
+        secundaryStage.setWidth(250);
+        secundaryStage.setHeight(75);
+        secundaryStage.setX(primaryStage.getX() + primaryStage.getWidth() / 2 - secundaryStage.getWidth() / 2);
+        secundaryStage.setY(primaryStage.getY() + primaryStage.getHeight() / 2 - secundaryStage.getHeight() / 2);
+        secundaryStage.setResizable(false);
+        secundaryStage.initModality(Modality.APPLICATION_MODAL);
+        secundaryStage.initStyle(StageStyle.UNDECORATED);
         secundaryStage.setScene(sceneCarga);
         secundaryStage.show();
         Platform.runLater(new Runnable() {
