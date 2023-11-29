2ea7ff5a79f4c1d38c8a2caf2ca50d191f7df216
MatiasRueda <matiasrued@gmail.com>
2023-11-29 10:06:36 -0300

    Refactorizada sala

--- src/main/java/cine/controller/Sala.java
+++ src/main/java/cine/controller/Sala.java
@@ -2,12 +2,15 @@ package cine.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import cine.model.Cine;
 import cine.model.Usuario;
 import cine.view.Butacas;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -15,7 +18,11 @@ public class Sala {
     private Cine cine = Controlador.cine;
     private Escenas escenas = Controlador.escenas;
     private Usuario usuario = Controlador.usuario;
-    private Butacas butacas;
+    private Butacas butacas = new Butacas();
+    private ArrayList<ArrayList<Integer>> reservas;
+    private Button anterior;
+    private Integer fila = -1;
+    private Integer columna = -1;
 
     @FXML
     private VBox sala;
@@ -28,14 +35,42 @@ public class Sala {
 
     @FXML
     void initialize() throws SQLException {
-        this.butacas = new Butacas(this.usuario, this.cine.getReservas());
-        this.butacas.setActionGridPane(this.butacasUno);
-        this.butacas.setActionGridPane(this.butacasDos);
+        this.reservas = this.cine.getReservas();
+        this.setActionGridPane(this.butacasUno);
+        this.setActionGridPane(this.butacasDos);
+    }
+    
+    private void action(Node node, Integer fila, Integer columna) {
+        if (this.anterior != null) 
+            butacas.colorDefault(anterior);
+        butacas.colorearElegido((Button) node);
+        this.fila = fila;
+        this.columna = columna;
+        this.anterior = (Button) node;
+        this.usuario.setFila(fila);
+        this.usuario.setColumna(columna);
+    }
+
+    public void setActionGridPane(GridPane gridpane) throws SQLException {
+        for (Node node:  gridpane.getChildren()) {
+            if (!(node instanceof Button)) 
+                continue;
+            Button boton = (Button) node;
+            Integer fila = GridPane.getRowIndex(node);
+            Integer columna = Integer.valueOf(boton.getText());
+            ArrayList<Integer> ubicacion = new ArrayList<>();
+            ubicacion.add(fila);
+            ubicacion.add(columna);
+            boton.setOnAction(e -> action(node, fila, columna));
+            if (!this.reservas.contains(ubicacion)) 
+                continue;
+            butacas.colorOcupada(boton);
+        }
     }
 
     @FXML
     void continuar(ActionEvent event) throws IOException, InterruptedException {
-        if (this.butacas.getFila() == -1 && this.butacas.getColumna() == -1) {
+        if (this.fila == -1 && this.columna == -1) {
             this.escenas.mensajeError("Elija alguna butaca");
             return;
         }
--- src/main/java/cine/view/Butacas.java
+++ src/main/java/cine/view/Butacas.java
@@ -1,58 +1,23 @@
 package cine.view;
 
-import java.util.ArrayList;
-
-import cine.model.Usuario;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 
 public class Butacas {
 
-    private Usuario usuario;
-    private ArrayList<ArrayList<Integer>> reservas;
-    private Button anteriorBtn;
     private String DEFAULT_STYLE = "-fx-border-color: black; -fx-text-fill: black; -fx-background-color: white;";
     private String SELECT_STYLE = "-fx-border-color: black; -fx-text-fill: white; -fx-background-color: green;";
-    private Integer fila = -1;
-    private Integer columna = -1;
 
-    public Butacas(Usuario usuario, ArrayList<ArrayList<Integer>> reservas) {
-        this.usuario = usuario;
-        this.reservas = reservas;
+    public void colorearElegido(Button butaca) {
+        butaca.setStyle(SELECT_STYLE);
     }
 
-    private void action(Node node, Integer fila, Integer columna) {
-        if (this.anteriorBtn != null) anteriorBtn.setStyle(this.DEFAULT_STYLE);
-        node.setStyle(this.SELECT_STYLE);
-        this.fila = fila;
-        this.columna = columna;
-        this.anteriorBtn = (Button) node;
-        this.usuario.setFila(fila);
-        this.usuario.setColumna(columna);
+    public void colorDefault(Button butaca) {
+        butaca.setStyle(DEFAULT_STYLE);
     }
 
-    public void setActionGridPane(GridPane gridpane) {
-        for (Node node:  gridpane.getChildren()) {
-            if (!(node instanceof Button)) continue;
-            Button boton = (Button) node;
-            Integer fila = GridPane.getRowIndex(node);
-            Integer columna = Integer.valueOf(boton.getText());
-            ArrayList<Integer> ubicacion = new ArrayList<>();
-            ubicacion.add(fila);
-            ubicacion.add(columna);
-            boton.setOnAction(e -> action(node, fila, columna));
-            if (!this.reservas.contains(ubicacion)) continue;
-            boton.setStyle("-fx-background-color: red");
-            boton.setDisable(true);
-        }
+    public void colorOcupada(Button butaca) {
+        butaca.setStyle("-fx-background-color: red");
+        butaca.setDisable(true);
     }
 
-    public Integer getColumna() {
-        return this.columna;
-    }
-
-    public Integer getFila() {
-        return this.fila;
-    }
 }
